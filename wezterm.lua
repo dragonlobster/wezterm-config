4a1b9ac745da414c56dcbb9f04db0faf4e4f01a2
dragonlobster <andrew@aftergate.io>
2024-06-13 13:08:12 +0800

    added prefix identifier

--- wezterm.lua
+++ wezterm.lua
@@ -7,13 +7,13 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Macchiato"
 config.font =
-  wezterm.font("JetBrains Mono NL")
+    wezterm.font("JetBrains Mono NL")
 config.font_size = 15
 
 config.window_decorations = "RESIZE"
@@ -43,12 +43,12 @@ config.keys = {
     },
     {
         mods = "LEADER",
-        key = "-",
+        key = "|",
         action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
     },
     {
         mods = "LEADER",
-        key = "|",
+        key = "-",
         action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
     },
     {
@@ -73,13 +73,13 @@ config.keys = {
     },
 }
 
-for i = 0, 8 do
-  -- leader + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(i),
-  })
+for i = 0, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i),
+    })
 end
 
 -- tab bar
@@ -87,6 +87,30 @@ config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
+
+-- tmux status
+wezterm.on("update-right-status", function(window, pane)
+    local SOLID_LEFT_ARROW = ""
+    local ARROW_FOREGROUND = { Foreground = { Color = "#ca9ee6" } }
+    local prefix = ""
+
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
+
+    if pane:pane_id() ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    end -- arrow color based on if tab is first pane
+
+    window:set_left_status(wezterm.format {
+        { Background = { Color = "#99d1db" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW }
+    })
+end)
+
 
 -- and finally, return the configuration to wezterm
 return config
